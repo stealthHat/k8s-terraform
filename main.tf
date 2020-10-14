5668ae6d64ceea674b666cb71479f591c386f0d0
stealthHat <blinklgui@gmail.com>
2020-10-14 14:45:12 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "sa-east-1"
+}
+
+resource "aws_instance" "nodes" {
+  ami           = "ami-02dc8ad50da58fffd"
+  instance_type = "t2.micro"
+  key_name      = "terraform-aws"
+  count         = 5
+  tags = {
+    Name = "node${count.index}"
+  }
+}
+
+resource "aws_security_group" "ssh-acess" {
+  name        = "ssh-acess"
+  description = "Allow ssh acess"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["191.249.65.42/32"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
