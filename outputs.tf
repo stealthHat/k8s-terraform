c1a7de4c0759bfe4c588cf92ab56e91a493514ee
stealthHat <blinklgui@gmail.com>
2020-11-14 04:35:21 -0300

    output of the ips

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,22 @@ output "aws_vpc" {
   value = module.aws-vpc.*
 }
 
+output "bastion_ip" {
+  value = join("\n", aws_instance.bastion-server.*.public_ip)
+}
+
+output "masters" {
+  value = join("\n", aws_instance.k8s-master.*.private_ip)
+}
+
+output "workers" {
+  value = join("\n", aws_instance.k8s-worker.*.private_ip)
+}
+
+output "etcd" {
+  value = join("\n", aws_instance.k8s-etcd.*.private_ip)
+}
+
 output "default_tags" {
   value = var.default_tags
 }
